29b68bc8337f6ce081fed2c87306cd8d8049e8c9
FIkri Afrizzi <fikri@fikri.arvitesar@gmail.com>
2019-12-13 16:05:06 +0700

    database config

--- config.php
+++ config.php
@@ -4,7 +4,7 @@ define('DB_USERNAME','root');
 define('DB_PASSWORD','');
 define('DB_NAME','DBHore');
 
-$mysqli = new mysqli(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_NAME);
+$mysqli = new $mysqli(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_NAME);
 if($mysqli === false){
 die("ERROR: Could not connect. ".$mysqli->connect_error);
 
